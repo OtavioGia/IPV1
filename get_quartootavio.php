411b5e892b1690bf18e23174b3f516cceca7c117
OtavioGia <otavio.giacomini@hotmail.com>
2025-05-14 08:28:19 -0300

    Update get_quartootavio.php

--- get_quartootavio.php
+++ get_quartootavio.php
@@ -2,3 +2,6 @@
 #EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_ODG
 #EXTBG:https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png
 http://cinepulse.live/get.php?username=688117510&password=249833520&type=m3u_plus
+#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Formiga
+#EXTBG:https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png
+https://np.tvdecasa.com.br/ssiptv/get/Formigatv/14042025/list_lives/
