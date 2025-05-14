62fa1c5e49c7ca4b43b943646ef03cb7792850b8
OtavioGia <otavio.giacomini@hotmail.com>
2025-05-13 22:52:06 -0300

    Update get_quartootavio.php

--- get_quartootavio.php
+++ get_quartootavio.php
@@ -1,1 +1,4 @@
-http://fiufiu.live:80/get.php?username=95817159&password=26254819&type=m3u_plus&output=hls
+#EXTM3U size="Big"
+#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Live
+#EXTBG:https://imgur.com/3dlcd3p.png
+https://fiufiu.live:80/get.php?username=95817159&password=26254819&type=m3u_plus&output=hls
