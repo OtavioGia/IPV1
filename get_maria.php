2056dec17bffcacbb6ead674de6fbdc21d1d5ce0
OtavioGia <otavio.giacomini@hotmail.com>
2025-05-15 10:47:53 -0300

    Update get_maria.php

--- get_maria.php
+++ get_maria.php
@@ -1,9 +1,15 @@
 # --- Usado Formiga Mais uma Achada ---
 
 #EXTM3U size="Big"
+
 #EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_ODG
 #EXTBG:https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png
 http://cinepulse.live/get.php?username=688117510&password=249833520&type=m3u_plus
+
 #EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Formiga
-#EXTBG:https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png
+#EXTBG:https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png
 https://np.tvdecasa.com.br/ssiptv/get/Formigatv/14042025/list_lives/
+
+#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Filmes_e_Series
+#EXTBG:https://en.wikipedia.org/wiki/Microsoft_Movies_%26_TV#/media/File:Movies_&_TV_logo.png
+http://dns.carnes.ink:8080/get.php?username=ZdEtSNbXb37d&password=xbb2ns98aprW&type=m3u_plus&output=m3u8
