76e92d24d62d9f3fe52648550511b208aab570f1
OtavioGia <otavio.giacomini@hotmail.com>
2025-05-16 10:03:43 -0300

    Update get_quartootavio.php

--- get_quartootavio.php
+++ get_quartootavio.php
@@ -6,10 +6,18 @@
 #EXTBG:https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png
 http://cinepulse.live/get.php?username=688117510&password=249833520&type=m3u_plus
 
-#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Formiga
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png",Lista_Formiga
 #EXTBG:https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png
 https://np.tvdecasa.com.br/ssiptv/get/Formigatv/14042025/list_lives/
 
-#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Filmes_e_Series
+#EXTINF:0 type="playlist" tvg-logo="https://en.wikipedia.org/wiki/Microsoft_Movies_%26_TV#/media/File:Movies_&_TV_logo.png",Lista_Filmes_e_Series
 #EXTBG:https://en.wikipedia.org/wiki/Microsoft_Movies_%26_TV#/media/File:Movies_&_TV_logo.png
 http://dns.carnes.ink:8080/get.php?username=ZdEtSNbXb37d&password=xbb2ns98aprW&type=m3u_plus&output=m3u8
+
+#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",TechSat1
+#EXTBG:https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png
+http://opbx.xyz/get.php?username=techsat&password=1234567&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",TechSat2
+#EXTBG:https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png
+http://vyvo.online/get.php?username=techsat&password=1234567&type=m3u_plus
