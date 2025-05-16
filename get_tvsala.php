e0943d9c9e8d2d1328e642ddd581c7c6985331ba
OtavioGia <otavio.giacomini@hotmail.com>
2025-05-16 10:19:55 -0300

    Update get_tvsala.php

--- get_tvsala.php
+++ get_tvsala.php
@@ -3,13 +3,16 @@
 #EXTM3U size="Big"
 
 #EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_ODG
-#EXTBG:https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png
 http://cinepulse.live/get.php?username=688117510&password=249833520&type=m3u_plus
 
-#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Formiga
-#EXTBG:https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png",Lista_Formiga
 https://np.tvdecasa.com.br/ssiptv/get/Formigatv/14042025/list_lives/
 
-#EXTINF:0 type="playlist" tvg-logo="https://imgur.com/bixCGMW.png",Lista_Filmes_e_Series
-#EXTBG:https://en.wikipedia.org/wiki/Microsoft_Movies_%26_TV#/media/File:Movies_&_TV_logo.png
+#EXTINF:0 type="playlist" tvg-logo="https://en.wikipedia.org/wiki/Microsoft_Movies_%26_TV#/media/File:Movies_&_TV_logo.png",Lista_Filmes_e_Series
 http://dns.carnes.ink:8080/get.php?username=ZdEtSNbXb37d&password=xbb2ns98aprW&type=m3u_plus&output=m3u8
+
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/HD_ready_1080p_logo.svg/1024px-HD_ready_1080p_logo.svg.png",TechSat1
+http://opbx.xyz/get.php?username=techsat&password=1234567&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://linorg.usp.br/xbmc/addons/omega/pvr.iptvsimple+windows-i686/icon.png",TechSat2
+http://vyvo.online/get.php?username=techsat&password=1234567&type=m3u_plus
