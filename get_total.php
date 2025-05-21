c56ba59e24df5588e4bbcc633e823620d320a573
OtavioGia <otavio.giacomini@hotmail.com>
2025-05-21 11:08:14 -0300

    Create get_total.php

---
+++ get_total.php
@@ -0,0 +1,27 @@
+# --- Usado Formiga Mais uma Achada ---
+
+#EXTM3U size="Big"
+
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/6/6f/IPTV.png ",Lista_ODG
+http://cinepulse.live/get.php?username=688117510&password=249833520&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Ant_motif.png/960px-Ant_motif.png",Lista_Formiga
+https://np.tvdecasa.com.br/ssiptv/get/Formigatv/14042025/list_lives/
+
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/en/4/4b/Movies_%26_TV_logo.png ",Lista_Filmes_e_Series
+http://dns.carnes.ink:8080/get.php?username=ZdEtSNbXb37d&password=xbb2ns98aprW&type=m3u_plus&output=m3u8
+
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/HD_ready_1080p_logo.svg/1024px-HD_ready_1080p_logo.svg.png",TechSat1
+http://opbx.xyz/get.php?username=techsat&password=1234567&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://linorg.usp.br/xbmc/addons/omega/pvr.iptvsimple+windows-i686/icon.png",TechSat2
+http://vyvo.online/get.php?username=techsat&password=1234567&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png",USA
+http://zorrohunter.com:8080/get.php?username=SoulAngelz&password=82661925527&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://w7.pngwing.com/pngs/629/920/png-transparent-grandparent-grandpa-grandpa-grandfather-hand-presentation-fictional-character-thumbnail.png",VO1
+https://otaviogia.github.io/IPV1/get_canais_vo1.php?username=VO&password=1233&type=m3u_plus
+
+#EXTINF:0 type="playlist" tvg-logo="https://w7.pngwing.com/pngs/629/920/png-transparent-grandparent-grandpa-grandpa-grandfather-hand-presentation-fictional-character-thumbnail.png",VO2
+https://otaviogia.github.io/IPV1/get_canais_vo2.php?username=VO&password=1233&type=m3u_plus
