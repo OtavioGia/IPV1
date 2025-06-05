9e2020303c61d5cff311a96cad583b7294d47e93
OtavioGia <otavio.giacomini@hotmail.com>
2025-06-05 10:32:42 -0300

    Update get_monteiro.php

--- get_monteiro.php
+++ get_monteiro.php
@@ -1,6 +1,68 @@
 # --- Entrou o formiga Maximo Aqui ---
 
 #EXTM3U
+#EXTINF:-1 tvg-id="PracaLagoa.trs" tvg-name="Praça Lagoa - Praça Alberto Teixeira da Rosa" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praça Lagoa - Praça Alberto Teixeira da Rosa
+https://video05.logicahost.com.br/camerateste01/camerateste01/chunklist_w1937444879.m3u8
+#EXTINF:-1 tvg-id="RotulaBomag.trs" tvg-name="Rotula Bomag - Rótula Barão x José Osório Cabral" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Rotula Bomag - Rótula Barão x José Osório Cabral
+https://video02.logicahost.com.br/rotulabarao/rotulabarao/chunklist_w1797052397.m3u8
+#EXTINF:-1 tvg-id="SalaoParoquial.trs" tvg-name="Salao Paroquial Av. Benjamin Constant x Firmíno Paim" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Salao Paroquial Av. Benjamin Constant x Firmíno Paim
+https://video02.logicahost.com.br/benjaminfirmino/benjaminfirmino/chunklist_w1142653019.m3u8
+#EXTINF:-1 tvg-id="PontePensil.trs" tvg-name="Ponte Pênsil" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Ponte Pênsil
+https://video02.logicahost.com.br/pensil/pensil/chunklist_w359480919.m3u8
+#EXTINF:-1 tvg-id="Kirra.trs" tvg-name="Kirra R. Joaquim Porto Kirra" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Kirra R. Joaquim Porto Kirra
+https://video02.logicahost.com.br/kirra/kirra/chunklist_w1928768387.m3u8
+#EXTINF:-1 tvg-id="QuatroPracas.trs" tvg-name="Quatro Praças" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Quatro Praças
+https://video02.logicahost.com.br/quatropraca/quatropraca/chunklist_w1834772855.m3u8
+#EXTINF:-1 tvg-id="PanoramicaIlhaLobos.trs" tvg-name="Panorâmica Ilha dos Lobos" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Panorâmica Ilha dos Lobos
+https://video02.logicahost.com.br/ilhalobo/ilhalobo/chunklist_w762512002.m3u8
+#EXTINF:-1 tvg-id="PracaPinheiroMachado.trs" tvg-name="Praça Pinheiro Machado" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praça Pinheiro Machado
+https://video02.logicahost.com.br/pracapinheiro/pracapinheiro/chunklist_w1575275941.m3u8
+#EXTINF:-1 tvg-id="PAAlexandrinoAlencar.trs" tvg-name="PA R. Alexandrino de Alencar" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",PA R. Alexandrino de Alencar
+https://video02.logicahost.com.br/cafepronto/cafepronto/chunklist_w1789592426.m3u8
+#EXTINF:-1 tvg-id="PostoSim.trs" tvg-name="Posto Sim Av. Castelo Branco x R. Luíz G. Capaverde" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Posto Sim Av. Castelo Branco x R. Luíz G. Capaverde
+https://video02.logicahost.com.br/autolugg/autolugg/chunklist_w287376927.m3u8
+#EXTINF:-1 tvg-id="PracaGetulioVargas.trs" tvg-name="Praça Getúlio Vargas" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praça Getúlio Vargas
+https://video02.logicahost.com.br/pracagetulio/pracagetulio/chunklist_w1500686591.m3u8
+#EXTINF:-1 tvg-id="BistekMammaBlend.trs" tvg-name="Bistek Mamma Blend - Praça de Alimentação" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Bistek Mamma Blend - Praça de Alimentação
+https://video02.logicahost.com.br/mammablend/mammablend/chunklist_w1420156898.m3u8
+#EXTINF:-1 tvg-id="ParqueBalonismo.trs" tvg-name="Parque do Balonismo" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Parque do Balonismo
+https://video02.logicahost.com.br/balon1/balon1/chunklist_w1801681162.m3u8
+#EXTINF:-1 tvg-id="PraiaParaiso.trs" tvg-name="Praia Paraíso" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praia Paraíso
+https://video02.logicahost.com.br/praiaparaiso/praiaparaiso/chunklist_w686283723.m3u8
+#EXTINF:-1 tvg-id="RioMampituba.trs" tvg-name="Rio Mampituba - Marina Paulo Prates" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Rio Mampituba - Marina Paulo Prates
+https://video02.logicahost.com.br/marinapp/marinapp/chunklist_w1186029213.m3u8
+#EXTINF:-1 tvg-id="RotulaBalao.trs" tvg-name="Rótula do Balão" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Rótula do Balão
+https://video02.logicahost.com.br/rotulabalao/rotulabalao/chunklist_w1805420711.m3u8
+#EXTINF:-1 tvg-id="PracaXV.trs" tvg-name="Praça XV" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praça XV
+https://video02.logicahost.com.br/pracaxv/pracaxv/chunklist_w1555763178.m3u8
+#EXTINF:-1 tvg-id="TabernaBistro.trs" tvg-name="Taberna Bistrô - R. Egídio Michaelsen" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Taberna Bistrô - R. Egídio Michaelsen
+https://video02.logicahost.com.br/taberna/taberna/chunklist_w2027891515.m3u8
+#EXTINF:-1 tvg-id="PraiaGuarita.trs" tvg-name="Praia da Guarita - (Guarita Gastronomia)" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praia da Guarita - (Guarita Gastronomia)
+https://video02.logicahost.com.br/guaritanew/guaritanew/chunklist_w1661587376.m3u8
+#EXTINF:-1 tvg-id="Prainha.trs" tvg-name="Prainha" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Prainha
+https://video02.logicahost.com.br/prainha/prainha/chunklist_w1002410998.m3u8
+#EXTINF:-1 tvg-id="OrlaGastronomica.trs" tvg-name="Orla Gastronômica" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Orla Gastronômica
+https://video02.logicahost.com.br/orlagastronomica1/orlagastronomica1/chunklist_w899639274.m3u8
+#EXTINF:-1 tvg-id="PraiaMolhes.trs" tvg-name="Praia dos Molhes" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praia dos Molhes
+https://video02.logicahost.com.br/molhes/molhes/chunklist_w1037402906.m3u8
+#EXTINF:-1 tvg-id="Stok.trs" tvg-name="Stok - Estrada do Mar x Av. Castelo Branco" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Stok - Estrada do Mar x Av. Castelo Branco
+https://video02.logicahost.com.br/estradadomar/estradadomar/chunklist_w181896055.m3u8
+#EXTINF:-1 tvg-id="PraiaCal.trs" tvg-name="Praia da Cal" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Praia da Cal
+https://video02.logicahost.com.br/praiadacal/praiadacal/chunklist_w751486774.m3u8
+#EXTINF:-1 tvg-id="Centro360.trs" tvg-name="Centro 360º" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Centro 360º
+https://video02.logicahost.com.br/360_maristela/360_maristela/chunklist_w1970838658.m3u8
+#EXTINF:-1 tvg-id="AeroportoPatioAeronaves.trs" tvg-name="Aeroporto - Pátio de Aeronaves" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Aeroporto - Pátio de Aeronaves
+https://video02.logicahost.com.br/aerocentral/aerocentral/chunklist_w278454603.m3u8
+#EXTINF:-1 tvg-id="AeroportoPatioNorte.trs" tvg-name="Aeroporto - Pátio Norte" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Aeroporto - Pátio Norte
+https://video02.logicahost.com.br/aeropationorte/aeropationorte/chunklist_w1393881816.m3u8
+#EXTINF:-1 tvg-id="AeroportoPatioSul.trs" tvg-name="Aeroporto - Pátio Sul" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Aeroporto - Pátio Sul
+https://video02.logicahost.com.br/aeropatiosul/aeropatiosul/chunklist_w618850839.m3u8
+#EXTINF:-1 tvg-id="AeroportoEntradaSaida.trs" tvg-name="Aeroporto - Entrada/Saída" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Aeroporto - Entrada/Saída
+https://video02.logicahost.com.br/aerofrente/aerofrente/chunklist_w931265936.m3u8
+#EXTINF:-1 tvg-id="AeroportoAcessoEstradaMar.trs" tvg-name="Aeroporto - Acesso Estrada do Mar" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Aeroporto - Acesso Estrada do Mar
+https://video02.logicahost.com.br/aeroacesso/aeroacesso/chunklist_w1852225934.m3u8
+#EXTINF:-1 tvg-id="AeroportoHangarRealizar.trs" tvg-name="Aeroporto - Hangar Realizar" tvg-logo="https://torres.rs.gov.br/wp-content/uploads/2025/03/brasao-cor.png" group-title="Cameras Ao Vivo Torres RS",Aeroporto - Hangar Realizar
+https://video02.logicahost.com.br/aerorealizar/aerorealizar/chunklist_w955637404.m3u8
 #EXTINF:-1 tvg-id="" tvg-name="GLOBO PRESIDENTE PRUDENTE HD°" tvg-logo="https://i.imgur.com/Fhjdpph.png" group-title="REDE GLOBO",GLOBO PRESIDENTE PRUDENTE HD°
 http://iron.cshunter.site:80/Formigatv/59629241/8472
 #EXTINF:-1 tvg-id="Recordmg.br" tvg-name="• RECORD MINAS SD" tvg-logo="https://i.imgur.com/smrVbFE.png" group-title="REDE RECORD",• RECORD MINAS SD
