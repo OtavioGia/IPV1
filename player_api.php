7a5044727082a31f79a50192024166b40949199d
OtavioGia <otavio.giacomini@hotmail.com>
2025-12-11 17:15:37 -0300

    Create player_api.php

---
+++ player_api.php
@@ -0,0 +1,25 @@
+{
+"user_info": {
+"username": "usuario",
+"password": "123",
+"message": "Login valido",
+"auth": 1,
+"status": "Active",
+"exp_date": "1999999999",
+"is_trial": "0",
+"active_cons": "0",
+"created_at": "1672531200",
+"max_connections": "1000",
+"allowed_output_formats": ["m3u8", "ts", "rtmp"]
+},
+"server_info": {
+"url": "otaviogia.github.io",
+"port": "80",
+"https_port": "443",
+"server_protocol": "https",
+"rtmp_port": "8888",
+"timezone": "America/Sao_Paulo",
+"timestamp_now": 1672531200,
+"time_now": "2023-01-01 00:00:00"
+}
+}
